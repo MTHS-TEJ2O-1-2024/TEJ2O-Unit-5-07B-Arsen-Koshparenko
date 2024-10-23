d0925035941c87a8cef0e57d944e7c76d1f8a1de
Arsen-Koshparenko <arsen.koshparenko@mths.ca>
2024-10-23 11:02:54 -0400

    servo motors

--- main.ts
+++ main.ts
@@ -1,8 +1,33 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Arsen Koshparenko
+ * Created on: Oct 2024
+ * This program turn servo to 0 and 180 degree
 */
 
-basic.showString('Hello, World!')
+//variables
+const servoNumber1 = robotbit.Servos.S1
+
+//setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+//button A is pressed
+input.onButtonPressed(Button.A, function() {
+    robotbit.Servo(servoNumber1, 0)
+    basic.clearScreen()
+    basic.showString("0")
+    basic.showIcon(IconNames.SmallSquare)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
+
+//button B is pressed
+input.onButtonPressed(Button.B, function () {
+    robotbit.Servo(servoNumber1, 180)
+    basic.clearScreen()
+    basic.showString("180")
+    basic.showIcon(IconNames.SmallSquare)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
